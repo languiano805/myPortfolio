58ec3eab5801d0839e3b7c5a411a14bc1a133ab0
Anguinao, Leonardo <languiano@csus.edu>
2025-05-21 15:49:04 -0700

    created basic outline for navbar

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,9 +1,15 @@
+import NavBar from "./components/NavBar";
+import "./App.css";
+
 function App() {
   return (
     <>
       <div className="app-container">
-        <div className="navbar-container"></div>
-        <div className="content-container"></div>
+        <div className="navbar-container">
+          <NavBar />
+        </div>
+        <div className="website-container"></div>
+        <div className="content-container">hello world</div>
       </div>
     </>
   );
---
+++ portfolio/src/components/NavBar.tsx
@@ -0,0 +1,15 @@
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <div className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Leonardo Anguiano
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
