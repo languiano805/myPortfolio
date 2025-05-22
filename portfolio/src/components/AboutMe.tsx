921f0d461c88ac784d483064854adca7ad9f3b73
Anguinao, Leonardo <languiano@csus.edu>
2025-05-21 20:21:59 -0700

    created a small pop up in the about me section

--- portfolio/src/components/AboutMe.tsx
+++ portfolio/src/components/AboutMe.tsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import mePhoto from "../assets/me-photo.jpeg";
 import "./AboutMe.css";
+import { TiArrowRightThick } from "react-icons/ti";
 
 const AboutMe = () => {
+  const [showPopup, setShowPopup] = useState(false);
   return (
     <div className="card about-me-container">
       <div className="card-img-wrapper">
@@ -12,7 +15,25 @@ const AboutMe = () => {
             <p className="card-text">
               Building my skills one line of code at a time.
             </p>
-            <div className="corner-square"></div>
+          </div>
+          <div className="corner-square">
+            <button
+              className="btn btn-primary"
+              onClick={() => setShowPopup(true)}
+            >
+              <TiArrowRightThick className="arrow-icon" />
+            </button>
+            {showPopup && (
+              <div className="popup">
+                <span>Hello, I'm Leonardo Anguiano!</span>
+                <button
+                  className="close-btn"
+                  onClick={() => setShowPopup(false)}
+                >
+                  Close
+                </button>
+              </div>
+            )}
           </div>
         </div>
       </div>
