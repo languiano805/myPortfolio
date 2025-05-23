72d3ddfe809dd4494f2e1c83868a85dd989f3aab
Anguinao, Leonardo <languiano@csus.edu>
2025-05-22 19:26:27 -0700

    downloading component is operational

--- portfolio/src/components/Resume.tsx
+++ portfolio/src/components/Resume.tsx
@@ -1,13 +1,14 @@
 import "./Resume.css";
+import resume from "../assets/leonardo_a_resume.pdf";
 
 const Resume = () => {
   return (
     <div className="resume-container">
       <div className="resume-text">Resume</div>
       <div className="cutout">
-        <div className="resume-download-container">
-            <div className="resume-download-text">Download</div>
-        </div>
+        <a className="resume-download-container" href={resume} download>
+          <div className="resume-download-text">Download</div>
+        </a>
       </div>
       <div className="resume-content ">
         B.S. in Computer Science from California State University, Sacramento.
